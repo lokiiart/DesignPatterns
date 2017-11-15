b25b1078f604c7a946575c38be61697105eb387e
lokiiart <lokiiart@gmail.com>
2017-11-15 12:54:20 +0800

    add  comments about factory and strategy

--- StrategyCashier/StrategyCashContext/CashContext.cs
+++ StrategyCashier/StrategyCashContext/CashContext.cs
@@ -15,6 +15,18 @@ namespace StrategyCashier.StrategyCashContext
             this.cs = csuper;
         }
 
+        //策略和工厂相结合，主要目的将判断放在Context中，这时候除了让客户端少认识一个类之外，没什么卵用
+        public CashContext(string type)
+        {
+            switch(type)
+            {
+                case "CashNormal":
+                    CashNormal CS0 = new CashNormal();
+                    cs = CS0;
+                    break;
+            }
+        }
+
         public double GetResult(double money)
         {
             return cs.AcceptCash(money);
